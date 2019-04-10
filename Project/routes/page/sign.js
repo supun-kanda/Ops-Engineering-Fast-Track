ad11810fad3ab30e349300410df14f22f9b0ba83
supun-kanda <supunkandambige9@gmail.com>
2019-04-10 19:45:42 +0530

    es5 files now serve

--- Project/routes/page/sign.js
+++ Project/routes/page/sign.js
@@ -2,7 +2,7 @@ var express = require('express'),
 router = express.Router(),
 path = require('path');
 
-router.get('/in',(req,res) => res.sendFile(path.join(__dirname,"../../public/html/index.html")));
-router.get('/up',(req,res) => res.sendFile(path.join(__dirname,"../../public/html/signup.html")));
+router.get('/in',(req,res) => res.sendFile(path.join(__dirname,"../../public/es5/html/index.html")));
+router.get('/up',(req,res) => res.sendFile(path.join(__dirname,"../../public/es5/html/signup.html")));
 
 module.exports = router;
