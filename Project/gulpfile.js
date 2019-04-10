4746c8184d48a0a60389810cdfa4820a0c08ae9f
supun-kanda <supunkandambige9@gmail.com>
2019-04-10 19:38:18 +0530

    add gulp babel

---
+++ Project/gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+babel = require('gulp-babel');
+
+gulp.task('full-conv',function(){
+    return gulp
+    .src(['./**/*.js','!./**/node_modules/**'])
+    .pipe(babel({presets: ['@babel/preset-env']}))
+    .pipe(gulp.dest('es5'));
+});
+gulp.task('full-copy',function(){
+    return gulp
+    .src(['./**','!./**/*.js','!./**/node_modules/**'])
+    .pipe(gulp.dest('es5'));
+});
+
+gulp.task('brows-conv',function(){
+    return gulp
+    .src(['./public/**/*.js','!./public/es5'])
+    .pipe(babel({presets: ['@babel/preset-env']}))
+    .pipe(gulp.dest('public/es5'));
+});
+gulp.task('brows-copy',function(){
+    return gulp
+    .src(['./public/**','!./public/**/*.js','!./public/es5'])
+    .pipe(gulp.dest('public/es5'));
+});
